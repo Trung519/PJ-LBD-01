1333fab7734aa411a2aa3017b10906a6c42b4b33
Quang Trung <trung.phamquang@hcmut.edu.vn>
2024-03-22 10:19:04 +0700

    add Ha`s layout add-event

--- src/Component/Navbar/component_addevent/EventName.jsx
+++ src/Component/Navbar/component_addevent/EventName.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react"
-
+import Input from "./Input,";
 export default function EventName() {
-    const [Header,setHeader] =useState("");
+    const [Header, setHeader] = useState("");
     return (
         <>
-            <h1 id="E-Header">{!Header?"------------":Header}</h1>
+            <h1 id="E-Header">{!Header ? "------------" : Header}</h1>
             <div id="E-name">
                 <div>
-                    <label id="event-name" >Tên sự kiện: </label>
-                    <input id="event-input" type="text" onChange={(e)=>{setHeader(e.target.value)}}></input>
+                    <Input label="Tên sự kiện: " id="event-input" type="text" onChange={(e) => { setHeader(e.target.value) }}></Input>
+                </div>
+
+                <div>
+                    <Input label="Người phụ trách:  " id="event-input" type="text" ></Input>
+                </div>
+                <div>
+                    <Input label="Thời gian diễn ra:   " id="event-input" type="date" ></Input>
                 </div>
                 <div>
                     <button id="btn-save">Save</button>
