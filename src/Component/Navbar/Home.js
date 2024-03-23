cd4f12e44e026fa660328473c09c5f56e85bff41
summer0404 <157554348+summer0404@users.noreply.github.com>
2024-03-23 20:55:50 +0700

    Update Home.js

--- src/Component/Navbar/Home.js
+++ src/Component/Navbar/Home.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react'
 import './Home.css'
-import imagedh from '../assets/Image_DH.png'
+import imagedh from '../assets/Image_DH.jpeg'
+import k22 from '../assets/k22.jpeg'
 import { render } from '@testing-library/react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import 'react-slideshow-image/dist/styles.css'
+import { Slide } from 'react-slideshow-image';
+
 function Func_Home() {
   const data = useLocation();
   const nav = useNavigate();
   return (
     <>
-      <div className='Layout_Home'>
-        <h1 id='titleHome'>Đoàn thanh niên <br /> Hội sinh viên <br /> Khoa KH&KT Máy tính</h1>
-        <img src={imagedh} id='imageDH'></img>
+      <Slide>
+        <div className="Layout_Home">
+      <img src={imagedh} alt="logo" className="imageDH" />
+      <div id="titleHome">Đoàn thanh niên <br /> Hội sinh viên <br /> Khoa KH&KT Máy tính</div>
+    </div>
+    <div className="Layout_Home">
+      <img src={k22} alt="k22" className="imageDH" />
+      <div id="K22title">
+        K22 <br /> MÃI ĐỈNH
       </div>
-      <div>
-        <h1 id='Notify'>Thông báo chung</h1>
-      </div>
+    </div>
+       
+        
+      </Slide>
+
     </>
   )
 }
 export default function Home() {
-  return(
+  return (
     <Func_Home />
   )
 } 
