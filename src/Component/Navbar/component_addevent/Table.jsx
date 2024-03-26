845590d23922da2ed656c7dd95a0c89ebb6246c5
summer0404 <157554348+summer0404@users.noreply.github.com>
2024-03-26 22:54:49 +0700

    change language to Vietnamese

---
+++ src/Component/Navbar/component_addevent/Table.jsx
@@ -0,0 +1,86 @@
+import { calculateMoney } from "../util/calculator";
+import VerifyButton from "./VerifyButton";
+import CommentField from "./CommentField";
+import { useEffect, useState } from 'react';
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+export default function Table({ tableData, onDeleteRow, onEdit, editingIndex }) {
+    const [allIsVerified, setAllIsVerified] = useState(false);
+    const [comments, setcomments] = useState([]);
+    const handleTrash = (index) => {
+        onDeleteRow(index);
+        const updatecmt = [...comments];
+        updatecmt.splice(index,1);
+        setcomments(updatecmt);
+        if (allIsVerified && tableData.length === 1) {
+            setAllIsVerified(false);
+        }
+    };
+    const handleEdit_Content = (index) => {
+        onEdit(index);
+    };
+
+    function handleVerifyAll() {
+        setAllIsVerified(allIsVerified => !allIsVerified);
+    }
+
+    const handleComment = (newComment, index) => {
+        const updatedComments = [...comments];
+        updatedComments[index] = newComment;
+        setcomments(updatedComments);
+    }
+
+    const row = tableData.map((data, index) => {
+        return (
+            <tr className={index===editingIndex?'editing-row':'DataTable'} key={index}>
+                <td>{index + 1} </td>
+                <td>{data.productName}</td>
+                <td>{data.productPrice}</td>
+                <td>{data.productQuantity}</td>
+                <td>{calculateMoney(data.productPrice, data.productQuantity)}</td>
+                <td>{comments[index]}</td>
+                <td>
+                    <VerifyButton allVerified={allIsVerified}/>
+                    </td>
+                <td id="latest-row">
+                    <div className="icon-table">
+                        <FontAwesomeIcon icon={faTrash} onClick={() => handleTrash(index)} />
+                        <FontAwesomeIcon icon={faPenToSquare} onClick={() => { handleEdit_Content(index) }} />
+                        <CommentField
+                            onChangeComment={(newComment) => handleComment(newComment, index)}
+                            comment={comments[index]}
+                        />
+                    </div>
+                </td>
+            </tr>
+        );
+    });
+
+    return (
+        <>
+            <table id="result">
+                <thead>
+                    <tr>
+                        <th>STT</th>
+                        <th>Tên sản phẩm</th>
+                        <th>Giá</th>
+                        <th>Số lượng</th>
+                        <th>Tổng tiền</th>
+                        <th>Ghi chú</th>
+                        <th>Tình trạng</th>
+                        <th>Hoạt động khác</th>
+                    </tr>
+                </thead>
+                <tbody id="table">
+                    {row}
+                </tbody>
+            </table>
+            <div className="container-vertifyall">
+                <button className="btn" onClick={handleVerifyAll}>Verify all</button>
+            </div>
+        </>
+    );
+}
---
+++ src/Component/Navbar/component_addevent/UserInput.jsx
@@ -0,0 +1,41 @@
+export default function UserInput({ userInput, onChange, onSubmit }) {
+
+    return (
+        <form id="user-input">
+            <div className="input-group">
+                <p>
+                    <label>Tên sản phẩm</label>
+
+                    <input type="text"
+                        required
+                        name="productName"
+                        value={userInput.productName}
+                        onChange={onChange}
+
+
+                    />
+
+                </p>
+                <p>
+                    <label>Giá</label>
+                    <input type="number"
+                        required
+                        name="productPrice"
+                        value={userInput.productPrice} onChange={onChange}
+                    />
+                </p>
+                <p>
+                    <label>Số lượng</label>
+                    <input type="number"
+                        required
+                        name="productQuantity"
+                        value={userInput.productQuantity}
+                        onChange={onChange} />
+                </p>
+                <div className="container-vertifyall">
+                    <button className="btn-sb-user-input" onClick={onSubmit}>Submit</button>
+                </div>
+            </div>
+        </form>
+    )
+}
